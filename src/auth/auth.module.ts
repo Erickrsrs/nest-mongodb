5c598e7df010b9ee768c10d6c1ecc12a082882f0
Erick Rocha <erick2003rocha@gmail.com>
2023-02-06 20:48:56 -0300

    create user model & schema and validation strategy with jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,28 @@
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersSchema } from '../users/schemas/users.schema';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UsersSchema,
+      },
+    ]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRATION,
+      },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
